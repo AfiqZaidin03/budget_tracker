501389cfffa661ad08fcd980a5bda9a474b83a6e
AfiqZaidin <76638759+AfiqZaidin@users.noreply.github.com>
2024-05-28 21:37:58 +0800

    [12] Create Dashboard, Incomes, Expenses
    Tab for Dashboard, Incomes, Expenses

---
+++ frontend/src/components/Incomes/Incomes.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+import { InnerLayout } from '../../styles/Layouts';
+
+
+function Incomes() {
+  return (
+    <IncomesStyled>
+        <InnerLayout>
+            
+        </InnerLayout>
+    </IncomesStyled>
+  )
+}
+
+const IncomesStyled = styled.div`
+`;
+
+export default Incomes
