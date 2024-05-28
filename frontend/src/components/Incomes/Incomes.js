7ae98626f81ffd531a102173512b5d1c010111fc
AfiqZaidin <76638759+AfiqZaidin@users.noreply.github.com>
2024-05-28 22:02:41 +0800

    [16] Add globalContext to incomes and GlobalProvider in index

--- frontend/src/components/Incomes/Incomes.js
+++ frontend/src/components/Incomes/Incomes.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useGlobalContext } from '../../context/globalContext';
 import { InnerLayout } from '../../styles/Layouts';
 
-
 function Incomes() {
-  return (
-    <IncomesStyled>
-        <InnerLayout>
-            
-        </InnerLayout>
-    </IncomesStyled>
-  )
-}
+    const {addIncome} = useGlobalContext()
+    return (
+        <IncomesStyled>
+            <InnerLayout>
+            <h1>Incomes</h1>
+                <div className="income-content">
+                    <div className="form-container"></div>
+                    <div className="incomes">
+                
+                    </div>
+                </div>
+            </InnerLayout>
+        </IncomesStyled>
+    )
+    }
 
 const IncomesStyled = styled.div`
 `;
