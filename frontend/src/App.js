2c12418a774a92a8f9217229d224c2bca9bc2269
AfiqZaidin <76638759+AfiqZaidin@users.noreply.github.com>
2024-05-28 21:39:03 +0800

    [13] setState for each navigation and style for main
    Plus displayData function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,41 @@
+import React, {useState, useMemo} from 'react';
 import styled from "styled-components";
 import bg from './images/bg.png';
 import { MainLayout } from './styles/Layouts';
 import Orb from './components/Orb/Orb'
 import Navigation from './components/Navigation/Navigation'
+import Dashboard from './components/Dashboard/Dashboard'
+import Incomes from './components/Incomes/Incomes'
+import Expenses from './components/Expenses/Expenses'
 
 function App() {
+  const [active, setActive] = React.useState(1)
+  const displayData = () => {
+    switch(active){
+      case 1:
+        return <Dashboard />
+      case 2:
+        return <Dashboard />
+      case 3:
+        return <Incomes />
+      case 4:
+        return <Expenses />
+      default :
+        return <Dashboard />
+    }
+  }
+
+  const orbMemo = useMemo(() => {
+    return <Orb />
+  }, [])
   return (
     <AppStyled bg={bg} className="App">
-      <Orb/>
+      {orbMemo}
       <MainLayout>
-        <Navigation />
+        <Navigation active={active} setActive={setActive} />
+        <main>
+
+        </main>
       </MainLayout>
     </AppStyled>
   );
@@ -19,6 +45,17 @@ const AppStyled = styled.div`
   height: 100vh;
   background-image: url(${props => props.bg}); 
   position: relative;
+  main{
+    flex: 1;
+    background: rgba(252, 246, 249, 0.78);
+    border: 3px solid #FFFFFF;
+    backdrop-filter: blur(4.5px);
+    border-radius: 32px;
+    overflow-x: hidden;
+    &::-webkit-scrollbar{
+      width: 0;
+    }
+  }
 `;
 
 export default App;
